2344d9f9cb21dca90636811f30f26e9389cba54e
ksi2564 <kimsungin5@gmail.com>
2022-04-07 02:06:48 +0900

    create labelling html

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task'
+    'task',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
