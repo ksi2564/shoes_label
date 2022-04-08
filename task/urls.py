a4b6da5110abcbf9916de0e97f323c0124ae2574
ksi2564 <kimsungin5@gmail.com>
2022-04-08 11:24:47 +0900

    create data_upload

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from task.views import task
+from task.views import task, uplaod_data
 
 app_name = "taskapp"
 
 urlpatterns = [
     path('', task, name='task'),
+    path('data_upload/', uplaod_data, name='upload_data'),
 ]
