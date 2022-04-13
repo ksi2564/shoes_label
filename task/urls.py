9cb09812d7c7dfb0bf91f6ea228eda9570e84fb3
leehyeongchan <gudcks1017@gmail.com>
2022-04-13 14:03:42 +0900

    라벨링 내 페이지

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from task.views import task, uplaod_data
+from task.views import task, uplaod_data, data_label, img_btn
 
 app_name = "taskapp"
 
 urlpatterns = [
     path('', task, name='task'),
     path('data_upload/', uplaod_data, name='upload_data'),
+    path('data_label/', data_label, name='data_label'),
+    path('img_btn/', img_btn, name='img_btn'),
 ]
--- task/views.py
+++ task/views.py
@@ -2,11 +2,31 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
 
 
 def task(request):
     return render(request, 'taskapp/task.html')
 
 
+def data_label(request):
+    return render(request, 'taskapp/data_label.html')
+
+
+def img_btn(request):
+    return render(request, 'teskapp/img_btn.html')
+
+
 def uplaod_data(request):
     return render(request, 'taskapp/data_upload.html')
+
+
+class Article:
+    pass
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'taskapp/list.html'
+    paginate_by = 6
