cac07110c841cd0d5ef9fcede226127d87697e48
ksi2564 <kimsungin5@gmail.com>
2022-06-03 18:12:27 +0900

    사용하지 않는 파일 정리

--- task/forms.py
+++
@@ -1,9 +0,0 @@
-# from django.forms import ModelForm
-#
-# from task.models import ShoesData
-#
-#
-# class ShoesDataCreationForm(ModelForm):
-#     class Meta:
-#         model = ShoesData
-#         fields = ['image']
