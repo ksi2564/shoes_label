5b3aa3851978f831af3fcf5167fe25d31e1d2430
ksi2564 <kimsungin5@gmail.com>
2022-06-08 18:16:19 +0900

    deploy

--- Capstone/settings/deploy.py
+++ Capstone/settings/deploy.py
@@ -1,4 +1,15 @@
 from .base import *
+import environ
+
+
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.split()
+    file.close()
+
+    return secret
+
 
 env = environ.Env(
     # set casting, default value
@@ -13,7 +24,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -29,7 +40,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'shoeslabel',
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
